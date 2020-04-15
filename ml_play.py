6220247080ba3923262b4a26f3f2e6c9216768ed
hankluo6 <hankluo6@gmail.com>
2020-04-15 12:33:22 +0800

    type error

--- ml_play.py
+++ ml_play.py
@@ -31,7 +31,7 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False    
-    filename = path.join(path.dirname(__file__), 'save', 'clf_random_forest_model.pickle.pickle')
+    filename = path.join(path.dirname(__file__), 'save', 'clf_random_forest_model.pickle')
     with open(filename, 'rb') as file:
         clf = pickle.load(file)
     filename = path.join(path.dirname(__file__), 'save', 'random_scaler.pickle')
